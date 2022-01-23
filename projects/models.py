f9283b1c088ca690cc7c1fa3d079d0ea544cabdd
shahin.mikayilov <shahin.mikayilov@dynatrace.com>
2022-01-23 16:47:47 +0100

    Add review and tags models; Update templates

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,4 @@
-from statistics import mode
 from django.db import models
-from django.forms import UUIDField
 import uuid
 
 # Create your models here.
@@ -9,5 +7,35 @@ class Project(models.Model):
     description  = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, blank=True, null=True)
+    vote_ratio = models.IntegerField(default=0, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4, unique=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote')
+    )
+    # owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4, unique=True)
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -2,35 +2,38 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Project
 
-projectList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce website',
-        'description': 'Ecommerce web site description'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio website',
-        'description': 'Portfolio web site description'
-    },
-    {
-        'id': '3',
-        'title': 'Social network',
-        'description': 'Desctiption of the project'
-    }    
-]
+# projectList = [
+#     {
+#         'id': '1',
+#         'title': 'Ecommerce website',
+#         'description': 'Ecommerce web site description'
+#     },
+#     {
+#         'id': '2',
+#         'title': 'Portfolio website',
+#         'description': 'Portfolio web site description'
+#     },
+#     {
+#         'id': '3',
+#         'title': 'Social network',
+#         'description': 'Desctiption of the project'
+#     }    
+# ]
 
-page = "projects"
-number = 15
-context = {"page": page, "number": number, "projects": projectList}
+# page = "projects"
+# number = 15
+
 
 def projects(request):
+    projectList = Project.objects.all()
+    context = {"projects": projectList}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id = pk)
+    # for i in projectList:
+    #     if i['id'] == pk:
+    #         projectObj = i
     return render(request, 'projects/single-project.html', {'project': projectObj})
