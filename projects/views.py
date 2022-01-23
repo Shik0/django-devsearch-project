eb90beb9e2f23ebe36c5fc70cea4507a6d10983e
shahin.mikayilov <shahin.mikayilov@dynatrace.com>
2022-01-23 17:15:09 +0100

    Add project form

--- projects/views.py
+++ projects/views.py
@@ -3,27 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
-# projectList = [
-#     {
-#         'id': '1',
-#         'title': 'Ecommerce website',
-#         'description': 'Ecommerce web site description'
-#     },
-#     {
-#         'id': '2',
-#         'title': 'Portfolio website',
-#         'description': 'Portfolio web site description'
-#     },
-#     {
-#         'id': '3',
-#         'title': 'Social network',
-#         'description': 'Desctiption of the project'
-#     }    
-# ]
 
-# page = "projects"
-# number = 15
 
 
 def projects(request):
@@ -33,7 +15,10 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id = pk)
-    # for i in projectList:
-    #     if i['id'] == pk:
-    #         projectObj = i
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    context = {'project': projectObj}
+    return render(request, 'projects/single_project.html', context)
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
