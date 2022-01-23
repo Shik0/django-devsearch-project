fc25bbac941d9693f3a1751639b225c8dac94458
shahin.mikayilov <shahin.mikayilov@dynatrace.com>
2022-01-23 11:34:37 +0100

    initial commit

---
+++ projects/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def projects(request):
+    return render(request, 'projects/projects.html')
+
+def project(request, pk):
+    return render(request, 'project/single-project.html')
