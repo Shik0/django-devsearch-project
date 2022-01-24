f856ae05b4c8262ec24ef3cd0f089fa404218b2c
shahin.mikayilov <shahin.mikayilov@dynatrace.com>
2022-01-24 17:03:53 +0100

    Implement CRUD

--- projects/forms.py
+++ projects/forms.py
@@ -4,4 +4,4 @@ from .views import Project
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = '__all__'
+        fields = ['title', 'description', 'demo_link','source_link', 'tags']
