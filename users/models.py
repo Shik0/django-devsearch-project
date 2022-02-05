ac52b5fb3a46fe87462876ea510e4844f822eafa
shahin.mikayilov <shahin.mikayilov@dynatrace.com>
2022-02-05 21:57:28 +0100

    Add signals

--- users/models.py
+++ users/models.py
@@ -1,17 +1,16 @@
-from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-# Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, null=True, blank=True)
     username = models.CharField(max_length=200, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     short_intro = models.CharField(max_length=500, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
-    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-profile.png')
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
     social_github = models.CharField(max_length=200, null=True, blank=True)
     social_twitter = models.CharField(max_length=200, null=True, blank=True)
     social_linkedin = models.CharField(max_length=200, null=True, blank=True)
@@ -21,5 +20,13 @@ class Profile(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4, unique=True)
 
 
+    def __str__(self):
+        return str(self.username)
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    
     def __str__(self):
         return str(self.name)
